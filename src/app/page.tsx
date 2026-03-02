9c392649e1c4e7c7d0b19cfe3f83f532fa5e509f
uniitee <ghildiyaluma0@gmail.com>
2026-03-02 21:48:00 +0530

    feat: add Features and Hero components to enhance homepage layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,13 @@
-import HeroSectionTextHover from "@/components/Hero";
+import Features from "@/components/hero/Features";
+import HeroSectionTextHover from "@/components/hero/Hero";
 import Image from "next/image";
 
-// export default function Home() {
-//   return (
-//     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-//       <main className="flex min-h-screen w-full max-w-7xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black">
-//         {/* <Nav /> */}
-//         <HeroSectionTextHover />
-//         {/* <StressRelief/> */}
-//       </main>
-//     </div>
-//   );
-// }
 export default function Home() {
   return (
     <div className="min-h-screen bg-zinc-50 font-sans dark:bg-black">
       <main className="relative h-[75vh] w-full">
         <HeroSectionTextHover />
+        <Features />
       </main>
     </div>
   );
---
+++ src/components/hero/Features.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Features = () => {
+  return (
+    <div>Features</div>
+  )
+}
+
+export default Features
+
+// remove this comment after editing
+// sections name:
+// { name: "Happiness meter", href: "/dashboard" },
+// { name: "Wellness", href: "/dashboard/wellness" },
+// { name: "Joy Missions", href: "/dashboard/missions" },
+// { name: "Analytics", href: "/dashboard/analytics" },
+
+// also hover mein gif mt daalna bs ig koi aur image daal dena ya maybe koi color <3
