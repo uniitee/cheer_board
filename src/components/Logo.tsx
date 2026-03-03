e474eb5592b50173ac4ddd37eebb39e8667f79b1
uniitee <ghildiyaluma0@gmail.com>
2026-03-03 11:02:09 +0530

    Refactor code structure for improved readability and maintainability

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,7 +4,7 @@ function Logo() {
   return (
     <div className="flex items-center gap-2 mb-4 ml-2.5">
       <Image
-        src="/basil.png"
+        src="/basil.svg"
         alt="Carefield Logo"
         width={32}
         height={32}
