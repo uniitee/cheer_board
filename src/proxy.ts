45258e99c397b9762d3b9e46831f460822161b45
uniitee <ghildiyaluma0@gmail.com>
2026-03-04 23:15:05 +0530

    feat: enhance authentication middleware to protect dashboard routes and redirect unauthenticated users

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,14 +1,16 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
-export default clerkMiddleware();
+export default clerkMiddleware(async (auth, req, event) => {
+  if (!(await auth()).userId && isProtectedRoute(req)) {
+    return (await auth()).redirectToSignIn({ returnBackUrl: req.url });
+  }
+});
 
 export const config = {
   matcher: [
-    /*
-     * Match all routes except:
-     * - _next
-     * - static files
-     */
-    "/((?!_next|.*\\..*).*)",
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+
+    "/(api|trpc)(.*)",
   ],
 };
